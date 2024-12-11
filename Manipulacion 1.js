b7a66958712d8ac8b4bb7e504ac4028cfbc8b370
albamirandacarrasco <amirandac05@educarex.es>
2024-12-11 18:55:21 +0100

    git

--- Manipulacion 1.js
+++ Manipulacion 1.js
@@ -2,5 +2,8 @@
 let coloresPrimarios = ["magenta", "azul", "amarillo"];
 
 coloresPrimarios.splice(1, 0, "morado");
+console.log("El array después de insertar un nuevo color:", coloresPrimarios);
 
-//
+//Extrae los último colores
+let ultimosColores = coloresPrimarios.slice(-2);
+console.log("Los dos últimos colores:", ultimosColores);
---
+++ Manipulacion 2.js
@@ -0,0 +1,12 @@
+//Tarea de manipulacion 2 Combina los arrays
+let nombres = ["Alba", "Pepe", "Luis"];
+let edades = [1, 33, 27];
+
+//concat
+let personas = nombres.concat(edades);
+console.log("Array combinado de nombres y edades:", personas);
+
+//join
+let cadenaPersonas = personas.join(", ");
+console.log("Array con los elementos combinados:", cadenaPersonas);
+
